90092b2533045050a925072a473f912c95eeea29
Rafael Montero <rafael.montero@gorillalogic.com>
2023-08-10 10:30:54 -0600

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <h1>Local Markdown Generator</h1>
+    <br>
+    <section class="markdown">
+      <textarea class="markdown-source" v-model="source"></textarea>
+      <br>
+      <hr class="dotted">
+      <br>
+      <div class="markdown-render" v-html="markdown.render(source)" />
+    </section>
+  </main>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref, watch, onMounted } from 'vue'
+import MarkdownIt from "markdown-it";
+import MarkdownItAbbr from "markdown-it-abbr";
+import MarkdownItHighlightjs from "markdown-it-highlightjs";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const markdown = new MarkdownIt()
+  .use(MarkdownItAbbr)
+  .use(MarkdownItHighlightjs),
+
+  source = ref('This is a test')
+
+onMounted(() => {
+  source.value = localStorage.getItem("mdSource");
+})
+
+watch(source, () => {
+  localStorage.setItem("mdSource", source.value);
+})
+
 </script>
 
 <style>
@@ -22,5 +42,26 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.markdown {
+  display: flex;
+  padding: 25px 0;
+}
+
+.markdown-source {
+  width: 35%;
+  height: 100vh;
+  border: none;
+}
+
+.markdown-render {
+  width: 65%;
+  height: 100vh;
+  text-align: left;
+}
+
+.dotted {
+  margin: 10px;
 }
 </style>
